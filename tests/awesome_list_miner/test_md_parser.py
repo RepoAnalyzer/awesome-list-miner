7ffd4723c53b62c761045ed38f0b1ad0527479d9
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-09 20:52:29 +0300

    feat(md_parser): added strictier pattern to match only github repos.

--- tests/awesome_list_miner/test_md_parser.py
+++ tests/awesome_list_miner/test_md_parser.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from awesome_list_miner.md_parser import find_all_links, is_web_href, md_to_html
+from awesome_list_miner.md_parser import find_all_links, is_github_repo_url, md_to_html
 
 script_location = Path(__file__).absolute().parent
 
@@ -12,15 +12,22 @@ def read(file_name: str) -> str:
     return f.read()
 
 
+simple_md = read("simple.md")
+
+
 @pytest.mark.parametrize(
     "markdown,links",
     [
         (
-            read("simple.md"),
+            simple_md,
             [
                 ("Table of Contents", "#table-of-contents"),
                 ("Simple", "#simple"),
                 ("folke/lazy.nvim", "https://github.com/folke/lazy.nvim"),
+                (
+                    "regular-expressions topic",
+                    "https://github.com/topics/regular-expressions?l=python",
+                ),
             ],
         ),
     ],
@@ -30,22 +37,36 @@ def test_find_all_links(markdown, links):
 
 
 @pytest.mark.parametrize(
+    "url,check_result",
+    [
+        ("#simple", False),
+        ("#table-of-contents", False),
+        # It's a website but not a github repo.
+        ("https://www.shorturl.at/shortener.php", False),
+        ("https://github.com/folke/lazy.nvim", True),
+        ("http://github.com/folke/lazy.nvim", True),
+        # It's a github page but not a repo.
+        ("https://github.com/topics/regular-expressions?l=python", False),
+    ],
+)
+def test_is_github_repo_url(url: str, check_result: bool):
+    assert is_github_repo_url(url) == check_result
+
+
+@pytest.mark.parametrize(
     "markdown,links",
     [
         (
-            read("simple.md"),
+            simple_md,
             [
                 ("folke/lazy.nvim", "https://github.com/folke/lazy.nvim"),
             ],
         ),
     ],
 )
-def test_find_all_web_links(markdown, links):
-    assert (
-        list(
-            filter(
-                lambda link: is_web_href(link[1]), find_all_links(md_to_html(markdown))
-            )
-        )
-        == links
+def test_find_all_github_repo_links(markdown, links):
+    github_repo_links = filter(
+        lambda link: is_github_repo_url(link[1]), find_all_links(md_to_html(markdown))
     )
+
+    assert list(github_repo_links) == links
